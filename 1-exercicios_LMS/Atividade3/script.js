3125ab1c5fc88dcaed1eef9177ad9d9c47fa6d70
Ruan Rocha <31362440+ruanrochaas@users.noreply.github.com>
2018-09-10 11:06:43 -0300

    Add files via upload

---
+++ 1-exercicios_LMS/Atividade3/script.js
@@ -0,0 +1,11 @@
+function prox(){
+    document.getElementById('vid1').style.display = 'none'; document.getElementById('vid2').style.display = 'block';
+}
+
+function ant(){
+    document.getElementById('vid2').style.display = 'none'; document.getElementById('vid1').style.display = 'block';
+}
+
+function cadst(){
+    alert('Cadastro realizado!');
+}
